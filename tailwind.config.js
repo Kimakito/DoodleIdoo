78d6017e6e558c2ab9d6f1283e4d91cdd9328eca
Charlotte <charlotte.lacroix@ikami.fr>
2025-06-16 10:25:56 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   presets: [require('@netlify/tailwind-config')],
   content: [
+    "./src/**/*.{html,js}",
     './*.html', // Assurez-vous que Tailwind scanne vos fichiers HTML
     // Ajoutez d'autres chemins si vous avez des fichiers JS/TS (ex: './src/**/*.{js,ts,jsx,tsx}')
   ],
