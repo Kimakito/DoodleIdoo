6b766a8add2140dd343c2755a6f5d5ff0df8142b
Charlotte <charlotte.lacroix@ikami.fr>
2025-06-17 10:19:39 +0200

    remove node, reinstall npm

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {}, // C'est ici que le changement doit être fait
-    autoprefixer: {}, // Gardez autoprefixer si vous l'utilisez
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
